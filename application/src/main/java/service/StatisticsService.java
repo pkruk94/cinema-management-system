b7fd7f2cc38bd6bc4e00bd2d373e9272c3d5f642
Pawel <pkruk94@gmail.com>
2020-11-17 17:37:25 +0100

    Added new service Added application.properties files

---
+++ application/src/main/java/service/StatisticsService.java
@@ -0,0 +1,24 @@
+package service;
+
+import discount.DiscountRepository;
+import lombok.RequiredArgsConstructor;
+import movie_showing.MovieShowingRepository;
+import order.OrderRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ticket.TicketRepository;
+import user.UserRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StatisticsService {
+
+    private final OrderRepository orderRepository;
+    private final TicketRepository ticketRepository;
+    private final MovieShowingRepository movieShowingRepository;
+    private final DiscountRepository discountRepository;
+    private final UserRepository userRepository;
+
+
+}
